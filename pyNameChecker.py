142a7d85a81af9b6cbd9015344c5ee9bf43d3931
Orpheus <sriramemailsyou@gmail.com>
2016-12-01 19:21:42 +0530

    Source Program Addded
    The addition of the main source program to execute the application.

---
+++ pyNameChecker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import MySQLdb
+
+def dbConnect():
+  cursor.execute('CREATE DATABASE IF NOT EXISTS python;')
+  cursor.execute("USE python")
+  cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT(6)  UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL);")	
+
+def dbLink(name):
+  cursor.execute("INSERT INTO users (firstname) VALUES (%s)",(name))
+  db.commit()
+
+def checkName(name):
+  flag = 0
+  cursor.execute("SELECT firstname FROM users")
+  data = cursor.fetchall()
+  for row in data :
+    if name == row["firstname"]:
+     flag = 1
+     break
+
+  if flag == 1 :
+    checkName = raw_input("Is your name " + name + "? ") 
+    if checkName.lower() == "yes":    
+     print "Hello, " + name  
+    else:    
+     name = raw_input("We're sorry about that. What is your name again?")
+     checkName(name)    
+  else :
+    dbLink(name)
+    print "Welcome aboard, "+ name
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="root")
+cursor = db.cursor(MySQLdb.cursors.DictCursor)
+dbConnect()
+uname = raw_input("Enter your name: ")
+checkName(uname)
